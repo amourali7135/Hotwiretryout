813eb500e0ccc90fdf2e1b36b0d6af11fa6eabd6
Amir Mourali <amourali7135@gmail.com>
2023-01-30 04:17:24 +0800

    More forms and routes for experimentation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,7 +3,52 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      flash[:notice] = 'Your restaurant was successfully created!'
+      redirect_to restaurants_path, method: :get
+    else
+      flash[:error] = 'There was an error, please try again!'
+      render 'new', status: :unprocessable_entity
+    end
+    # raise
+  end
+
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new  # Add this line
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      flash[:notice] = "You've successfully updated your restaurant!"
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      flash[:error] = "There was an error, please try again!"
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = "You've successfully deleted this restaurant!"
+    redirect_to restaurants_path
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:address, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  resources :restaurants, only: [ :index, :show ] do
+  resources :restaurants do #, only: [ :index, :show, :new, :create, :update, :edit,  ] do
     resources :reviews, only: :create
   end
 end
