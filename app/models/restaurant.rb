8a570e8cc511e6730a99ad55d220afeb75244c43
Amir Mourali <amourali7135@gmail.com>
2023-02-05 01:47:05 +0800

    Action cable ajax added and functional

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,27 @@
 class Restaurant < ApplicationRecord
+  # after_create_commit -> { broadcast_prepend_to "restaurants", partial: "restaurants/restaurant", locals: { restaurant: self }, target: "restaurants" }
+  
   validates :name, presence: true
   has_many :reviews, dependent: :destroy
 
   scope :ordered, -> { order(id: :desc)}
+
+
+  # after_create_commit -> { broadcast_prepend_to "restaurants", partial: "restaurants/restaurant", locals: { restaurant: self }, target: "restaurants" }
+
+  # after_create_commit -> { broadcast_prepend_to "restaurants", partial: "restaurants/restaurant", locals: { restaurant: self } }
+
+  # after_create_commit -> { broadcast_prepend_to "restaurants" }
+  # after_update_commit -> { broadcast_replace_to "restaurants" }
+  # after_destroy_commit -> { broadcast_remove_to "restaurants" }
+  
+  # for sidekiq
+  # after_create_commit -> { broadcast_prepend_later_to "restaurants" }
+  # after_update_commit -> { broadcast_replace_later_to "restaurants" }
+  # after_destroy_commit -> { broadcast_remove_to "restaurants" }
+  # ultimate refactoring 
+  broadcasts_to ->(restaurant) { "restaurants" }, inserts_by: :prepend
+
+
+
 end
